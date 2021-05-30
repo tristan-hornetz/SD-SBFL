7fe535f2525356409d479d23133417ed734ff686
Tristan Hornetz <s8trhorn@stud.uni-saarland.de>
2021-05-30 16:16:40 +0200

    Makefile now automatically creates a pyenv environment for you

--- Makefile
+++ Makefile
@@ -1,10 +1,66 @@
 REPO_DIR := $(shell pwd)/_BugsInPy
 REPO_URI="https://github.com/tristan-hornetz/BugsInPy"
 DEBUGGER_SYMDIR=_root
-
+ENV_SCRIPT=.env.sh
 PY_FILES := $(wildcard *.py)
 
-all: BugsInPy_Repository symlinks
+sh_start=10
+sh_end=59
+define create_environment =
+#!/bin/bash
+###Initialize pyenv
+
+python_version=3.9.2
+env_name=statistical_debugger
+
+eval "$(pyenv init -)"
+eval "$(pyenv virtualenv-init -)"
+
+
+###Install required python version if not already installed
+python_installed="FALSE"
+while read -r line
+do
+  if [[ $line == "${python_version}" ]]; then
+    python_installed="TRUE"
+    echo "Python Version ${python_version} is already installed."
+    break
+  fi
+done < <(pyenv versions)
+
+if [[ $python_installed == "FALSE" ]]; then
+  pyenv install $python_version
+fi
+
+###Check if environment already present
+env_present="0"
+while read -r line
+do
+  if [[ $line == "${env_name}"* ]]; then
+    env_present="1"
+    break
+  fi
+done < <(pyenv virtualenvs)
+
+
+if [[ $env_present == "0" ]]; then
+  ###Create environment
+  echo "Creating environment ${env_name}..."
+  pyenv virtualenv "$python_version" "$env_name"
+fi
+
+###Activate environment
+pyenv activate "$env_name"
+
+### Install requirements
+pip install -r requirements.txt
+echo "pyenv activate $env_name" > activate
+pyenv deactivate
+
+endef
+
+
+all: BugsInPy_Repository symlinks environment
 
 BugsInPy_Repository:
 	git clone "$(REPO_URI)" "$(REPO_DIR)"
@@ -15,13 +71,25 @@ symdir:
 %.py: symdir
 	ln -s "$(shell pwd)/$@" "$(DEBUGGER_SYMDIR)/$@"
 
+environment:
+
 
 symlinks: $(PY_FILES)
 	ln -s "$(REPO_DIR)/framework/py/TestWrapper" "$(shell pwd)/TestWrapper"
 	ln -s "$(shell pwd)/debuggingbook" "$(shell pwd)/$(DEBUGGER_SYMDIR)/debuggingbook"
 	ln -s "$(shell pwd)/$(DEBUGGER_SYMDIR)" "$(REPO_DIR)/framework/py/TestWrapper/root"
 
+
+environment:
+	@sed '$(sh_start),$(sh_end)!d' Makefile > $(ENV_SCRIPT)
+	@chmod +x $(ENV_SCRIPT)
+	@./$(ENV_SCRIPT)
+	@rm $(ENV_SCRIPT)
+
 clean:
 	rm -rf "$(DEBUGGER_SYMDIR)"
 	rm -f "$(shell pwd)/TestWrapper"
 	rm -rf $(REPO_DIR)
+	rm -f $(ENV_SCRIPT)
+	rm -f activate
+	pyenv virtualenv-delete -f statistical_debugger
--- evaluate.py
+++ evaluate.py
@@ -6,36 +6,66 @@ import pickle
 
 MATCH_STRING = "tornado/http1connection.py"
 
-DEFAULT_INPUT = os.path.dirname(os.path.abspath(sys.argv[0])) + "/TestWrapper/results.pickle.gz"
 DEFAULT_INFODIR = "."
 
-arg_parser = argparse.ArgumentParser(description='Evaluate fault localization results.')
-arg_parser.add_argument("-r", "--result_file", required=False, type=str, default=DEFAULT_INPUT, help="The file conataining test results")
-arg_parser.add_argument("-i", "--info_dir", required=False, type=str, default=DEFAULT_INFODIR, help="The BugsInPy Project Directory containig information about the specific bug")
 
-args = arg_parser.parse_args()
-
-
-with gzip.open(args.result_file) as f:
-    result_container = pickle.load(f)
-
-results = result_container.results
-bug_id = result_container.bug_id
-project_name = result_container.project_name
+def get_info_directory(_results):
+    base = _results.work_dir.split("_BugsInPy")[0]
+    return f"{base}_BugsInPy/projects/{_results.project_name}/bugs/{_results.bug_id}"
 
 
-print(f"Results for {project_name}, Bug {bug_id}")
-print(f"Ranked {len(results)} Events")
-print(f"Most suspicious:\n{results[0]}\n")
+class BugInfo:
+    def __init__(self, _results):
+        with open(get_info_directory(_results) + "/bug.info", "rt") as f:
+            self.attrs = list()
+            while True:
+                line = f.readline()
+                if not line:
+                    break
+                if "=" in line:
+                    attr = line.split("=", 1)[0]
+                    val = line.split("=", 1)[1].strip("\" \n")
+                    setattr(self, attr, val)
+                    self.attrs.append(attr)
 
-matchstring_index = -1
-matchstring_item = ("", 0)
-for item, lineno in results:
-    matchstring_index += 1
-    if MATCH_STRING in item:
-        matchstring_item = (item, lineno)
-        break
+    def __str__(self):
+        ret = ""
+        for a in self.attrs:
+            ret += f"{a}=\"{getattr(self, a)}\"\n"
+        return ret
 
-print(f"Most suspicious occurrence of matchstring module: Rank #{matchstring_index}, Top {matchstring_index*100.0/len(results)}%")
-print(matchstring_item)
 
+if __name__ == "__main__":
+
+    DEFAULT_INPUT = os.path.dirname(os.path.abspath(sys.argv[0])) + "/TestWrapper/results.pickle.gz"
+
+    arg_parser = argparse.ArgumentParser(description='Evaluate fault localization results.')
+    arg_parser.add_argument("-r", "--result_file", required=False, type=str, default=DEFAULT_INPUT,
+                            help="The file conataining test results")
+    arg_parser.add_argument("-i", "--info_dir", required=False, type=str, default=DEFAULT_INFODIR,
+                            help="The BugsInPy Project Directory containig information about the specific bug")
+
+    args = arg_parser.parse_args()
+
+    with gzip.open(args.result_file) as f:
+        result_container = pickle.load(f)
+
+    results = result_container.results
+    bug_id = result_container.bug_id
+    project_name = result_container.project_name
+
+    print(f"Results for {project_name}, Bug {bug_id}")
+    print(f"Ranked {len(results)} Events")
+    print(f"Most suspicious:\n{results[0]}\n")
+
+    matchstring_index = -1
+    matchstring_item = ("", 0)
+    for item, lineno in results:
+        matchstring_index += 1
+        if MATCH_STRING in item:
+            matchstring_item = (item, lineno)
+            break
+
+    print(
+        f"Most suspicious occurrence of matchstring module: Rank #{matchstring_index}, Top {matchstring_index * 100.0 / len(results)}%")
+    print(matchstring_item)
