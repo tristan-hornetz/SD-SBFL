17302061dc882a7447d66e9698942fd38efedeb7
Tristan Hornetz <s8trhorn@stud.uni-saarland.de>
2021-06-07 21:14:44 +0200

    Performance upgrades

--- Collector.py
+++ Collector.py
@@ -1,6 +1,6 @@
 import inspect
 from types import FrameType, FunctionType
-from typing import Any, Set, Tuple
+from typing import Any, Set, Tuple, Optional, Callable
 
 from TestWrapper.root.debuggingbook.StatisticalDebugger import CoverageCollector
 
@@ -18,26 +18,54 @@ class ExtendedCoverageCollector(CoverageCollector):
     """
 
     def __init__(self, *args, **kwargs):
+        self.exclude_function_dict = dict()
+
         super(ExtendedCoverageCollector, self).__init__(*args, **kwargs)
         with open(inspect.getfile(self.__init__).split("/TestWrapper/")[0] + "/TestWrapper/work_dir.info", "rt") as f:
             self.work_dir_base = str(f.readline().replace("\n", ""))
 
-    def collect(self, frame: FrameType, event: str, arg: Any) -> None:
-        """
-        Same as CoverageCollector::collect, but with a more elaborate method of filtering out unimportant events
-        """
-
+        self.ignore_types = list(filter(lambda e: isinstance(e, type), self.items_to_ignore))
+        self.ignore_names = set(e.__name__ for e in filter(lambda e: hasattr(e, '__name__'), self.items_to_ignore))
         # Exclude events from file paths with these substrings:
-        to_exclude = ["/TestWrapper/", "/test_", "_test.py", "/WrapClass.py"]
-        name = frame.f_code.co_name
-        function = self.search_func(name, frame)
+        self.to_exclude = ["/TestWrapper/", "/test_", "_test.py", "/WrapClass.py"]
 
-        if function is None:
-            function = self.create_function(frame)
+
+    def traceit(self, frame: FrameType, event: str, arg: Any) -> None:
+        """
+        Tracing function.
+        Saves the first function and calls collect().
+        """
+        if self.ignore_items:
+            if frame.f_code.co_name in self.ignore_names:
+                return
+            if 'self' in frame.f_locals:
+                for t in self.ignore_types:
+                    if isinstance(frame.f_locals['self'], t):
+                        return
+
+        if self._function is None and event == 'call':
+            # Save function
+            self._function = self.create_function(frame)
+            self._args = frame.f_locals.copy()
+            self._argstring = ", ".join([f"{var}={repr(self._args[var])}"
+                                         for var in self._args])
+
+        self.collect(frame, event, arg)
+
+    def exclude_function(self, function):
+        """
+        Should the given function be excluded from collection?
+        :param function: A function encountered by traceit
+        :return: True if the given function should be excluded from collection
+        """
+
+        if function in self.exclude_function_dict.keys():
+            return self.exclude_function_dict[function]
 
         # ONLY collect functions, no other garbage
         if not isinstance(function, FunctionType):
-            return
+            self.exclude_function_dict[function] = True
+            return True
 
         function_filename = get_file_resistant(function)
 
@@ -51,11 +79,48 @@ class ExtendedCoverageCollector(CoverageCollector):
 
         # Don't collect function which are defined outside of our project
         if self.work_dir_base not in function_filename:
-            return
+            self.exclude_function_dict[function] = True
+            return True
 
-        for s in to_exclude:
+        for s in self.to_exclude:
             if s in function_filename:
-                return
+                self.exclude_function_dict[function] = True
+                return True
+
+        self.exclude_function_dict[function] = False
+        return False
+
+    def search_func(self, name: str, frame: Optional[FrameType] = None) -> \
+        Optional[Callable]:
+        """Search in callers for a definition of the function `name`"""
+        if frame is None:
+            frame = self.caller_frame()
+
+        try:
+            item = None
+            if name in frame.f_globals:
+                item = frame.f_globals[name]
+            if name in frame.f_locals:
+                item = frame.f_locals[name]
+            if item and callable(item):
+                return item
+        except Exception:
+            pass
+        return None
+
+    def collect(self, frame: FrameType, event: str, arg: Any) -> None:
+        """
+        Same as CoverageCollector::collect, but with a more elaborate method of filtering out unimportant events
+        """
+
+        name = frame.f_code.co_name
+        function = self.search_func(name, frame)
+
+        if function is None:
+            function = self.create_function(frame)
+
+        if self.exclude_function(function):
+            return
 
         location = (function, frame.f_lineno)
         self._coverage.add(location)
--- run_test.py
+++ run_test.py
@@ -1,12 +1,12 @@
 import os
 import sys
-
+if __name__ == '__main__' and (not os.path.islink(os.path.abspath(os.path.dirname(sys.argv[0])) + '/TestWrapper')):
+    print('Symlinks not found. Did you run make?')
+    exit(-1)
 from TestWrapper.root.Debugger import debugger, SFL_Results
 from TestWrapper.root.evaluate import BugInfo
-
-dump_file = os.path.curdir + "/TestWrapper/results.pickle.gz"
+dump_file = os.path.curdir + '/TestWrapper/results.pickle.gz'
 test_ids = []
-
 
 def get_test_ids():
     """
@@ -16,16 +16,15 @@ def get_test_ids():
     _results = SFL_Results(debugger)
     _info = BugInfo(_results)
     ret = []
-    with open(_info.info_dir + "/run_test.sh", "rt") as f:
+    with open(_info.info_dir + '/run_test.sh', 'rt') as f:
         while True:
             line = f.readline()
             if not line:
                 break
-            run_test_sh = line.strip(" \n")
-            if run_test_sh.startswith("python") or run_test_sh.startswith("pytest ") or run_test_sh.startswith("tox"):
-                ret.append(list(filter(lambda s: not s.startswith("-"), run_test_sh.split(" "))).pop())
+            run_test_sh = line.strip(' \n')
+            if run_test_sh.startswith('python') or run_test_sh.startswith('pytest ') or run_test_sh.startswith('tox'):
+                ret.append(list(filter(lambda s: not s.startswith('-'), run_test_sh.split(' '))).pop())
     return ret
-
 
 def run_test(root_dir: str, project: str, bug_id: int):
     """
@@ -35,36 +34,25 @@ def run_test(root_dir: str, project: str, bug_id: int):
     :param bug_id: The numerical Bug ID
     :return: None
     """
-    binary_dir = root_dir + "/_BugsInPy/framework/bin"
-    work_dir = os.path.abspath(binary_dir + "/temp/" + project)
-    debugger_module = os.path.abspath(root_dir + "/run_test.py")
-
-    os.system(f"{binary_dir}/bugsinpy-checkout -p {project} -i {bug_id} -v 0")
-    os.system(f"{binary_dir}/bugsinpy-compile -w {work_dir}")
-    os.system(f"{binary_dir}/bugsinpy-instrument -c {debugger_module} -w {work_dir}")
-    os.system(f"{binary_dir}/bugsinpy-test -a -w {work_dir}")
-
-
+    binary_dir = root_dir + '/_BugsInPy/framework/bin'
+    work_dir = os.path.abspath(binary_dir + '/temp/' + project)
+    debugger_module = os.path.abspath(root_dir + '/run_test.py')
+    os.system(f'{binary_dir}/bugsinpy-checkout -p {project} -i {bug_id} -v 0')
+    os.system(f'{binary_dir}/bugsinpy-compile -w {work_dir}')
+    os.system(f'{binary_dir}/bugsinpy-instrument -c {debugger_module} -w {work_dir}')
+    os.system(f'{binary_dir}/bugsinpy-test -a -w {work_dir}')
 if __name__ == '__main__':
     from evaluate import SFL_Evaluation
     import argparse
-
     arg_parser = argparse.ArgumentParser(description='Run the debugger on a specific bug')
-    arg_parser.add_argument("-p", "--project_name", required=True, type=str, default="thefuck",
-                            help="The name of the target project")
-    arg_parser.add_argument("-i", "--bug_id", required=True, type=int, default=2,
-                            help="The numerical ID of the target bug")
-
+    arg_parser.add_argument('-p', '--project_name', required=True, type=str, default='thefuck', help='The name of the target project')
+    arg_parser.add_argument('-i', '--bug_id', required=True, type=int, default=2, help='The numerical ID of the target bug')
     args = arg_parser.parse_args()
-
     root_dir = os.path.dirname(os.path.abspath(sys.argv[0]))
     project = args.project_name
     bug_id = args.bug_id
-
     run_test(root_dir, project, bug_id)
-
     print(SFL_Evaluation(dump_file))
-
 else:
     debugger.dump_file = dump_file
     test_ids.extend(get_test_ids())
