260ca3aa2a7a450e807f522c3ee4112151e20593
Tristan Hornetz <s8trhorn@stud.uni-saarland.de>
2021-08-02 16:51:39 +0200

    Added new similarity coefficients

--- Evaluator/SimilarityCoefficient.py
+++ Evaluator/SimilarityCoefficient.py
@@ -1,9 +1,20 @@
 import math
 from abc import abstractmethod
+
 from .RankerEvent import RankerEvent
 
 
 class SimilarityCoefficient:
+    @staticmethod
+    def get_values(event):
+        failed = len(event.failed_with_event)
+        passed = len(event.passed_with_event)
+        total_failed = event.total_failed
+        total_passed = event.total_passed
+        not_in_failed = total_failed - failed
+        not_in_passed = total_passed - passed
+        return passed, failed, not_in_passed, not_in_failed, total_passed, total_failed
+
     @staticmethod
     @abstractmethod
     def compute(event: RankerEvent):
@@ -12,12 +23,100 @@ class SimilarityCoefficient:
 
 class OchiaiCoefficient(SimilarityCoefficient):
     @staticmethod
-    @abstractmethod
     def compute(event: RankerEvent):
-        failed = len(event.failed_with_event)
-        passed = len(event.passed_with_event)
-        total_failed = event.total_failed
+        passed, failed, not_in_passed, not_in_failed, total_passed, total_failed = SimilarityCoefficient.get_values(
+            event)
         try:
             return failed / math.sqrt(total_failed * (failed + passed))
+        except ZeroDivisionError:
+            return 0.0
+
+
+class JaccardCoefficient(SimilarityCoefficient):
+    @staticmethod
+    def compute(event: RankerEvent):
+        passed, failed, not_in_passed, not_in_failed, total_passed, total_failed = SimilarityCoefficient.get_values(
+            event)
+        try:
+            return failed / (total_failed + passed)
+        except ZeroDivisionError:
+            return 0.0
+
+
+class SorensenDiceCoefficient(SimilarityCoefficient):
+    @staticmethod
+    def compute(event: RankerEvent):
+        passed, failed, not_in_passed, not_in_failed, total_passed, total_failed = SimilarityCoefficient.get_values(
+            event)
+        try:
+            return (2.0 * failed) / ((2.0 * failed) + not_in_failed + passed)
+        except ZeroDivisionError:
+            return 0.0
+
+
+class AnderbergCoefficient(SimilarityCoefficient):
+    @staticmethod
+    def compute(event: RankerEvent):
+        passed, failed, not_in_passed, not_in_failed, total_passed, total_failed = SimilarityCoefficient.get_values(
+            event)
+        try:
+            return failed / (failed + 2 * (not_in_failed + passed))
+        except ZeroDivisionError:
+            return 0.0
+
+
+class SimpleMatchingCoefficient(SimilarityCoefficient):
+    @staticmethod
+    @abstractmethod
+    def compute(event: RankerEvent):
+        passed, failed, not_in_passed, not_in_failed, total_passed, total_failed = SimilarityCoefficient.get_values(
+            event)
+        try:
+            return (failed + not_in_passed) / (total_failed + event.total_passed)
+        except ZeroDivisionError:
+            return 0.0
+
+
+class RogersTanimotoCoefficient(SimilarityCoefficient):
+    @staticmethod
+    def compute(event: RankerEvent):
+        passed, failed, not_in_passed, not_in_failed, total_passed, total_failed = SimilarityCoefficient.get_values(
+            event)
+        try:
+            return (failed + not_in_passed) / (failed + not_in_passed + 2 * (not_in_failed + passed))
+        except ZeroDivisionError:
+            return 0.0
+
+
+class OchiaiIICoefficient(SimilarityCoefficient):
+    @staticmethod
+    def compute(event: RankerEvent):
+        passed, failed, not_in_passed, not_in_failed, total_passed, total_failed = SimilarityCoefficient.get_values(
+            event)
+        try:
+            return (failed * not_in_passed) / math.sqrt(
+                total_failed * (failed + passed) * (not_in_failed + not_in_passed) * total_passed)
+        except ZeroDivisionError:
+            return 0.0
+
+
+class RusselRaoCoefficient(SimilarityCoefficient):
+    @staticmethod
+    def compute(event: RankerEvent):
+        passed, failed, not_in_passed, not_in_failed, total_passed, total_failed = SimilarityCoefficient.get_values(
+            event)
+        try:
+            return failed / (total_failed + total_passed)
+        except ZeroDivisionError:
+            return 0.0
+
+
+class TarantulaCoefficient(SimilarityCoefficient):
+    @staticmethod
+    def compute(event: RankerEvent):
+        passed, failed, not_in_passed, not_in_failed, total_passed, total_failed = SimilarityCoefficient.get_values(
+            event)
+        try:
+            return (failed / total_failed) / ((passed / total_passed) + (failed / total_failed))
         except ZeroDivisionError:
             return 0.0
--- evaluate.py
+++ evaluate.py
@@ -1,15 +1,48 @@
 import argparse
 import os
+from os.path import dirname, exists, abspath
 import sys
 import gzip
 import pickle
 
-from Evaluator.CombiningMethod import GenericCombiningMethod, avg, inv_arg, WeightedCombiningMethod
+from Evaluator.CombiningMethod import CombiningMethod, GenericCombiningMethod, avg, inv_arg, WeightedCombiningMethod
 from Evaluator.SimilarityCoefficient import OchiaiCoefficient
 from Evaluator.RankerEvent import SDBranchEvent, LineCoveredEvent, SDReturnValueEvent, SDScalarPairEvent
 from Evaluator.Evaluation import MetaEvaluation
+from Evaluator.CodeInspection.utils import mkdirRecursive
 
 THREADS = os.cpu_count() - 2
+
+
+def create_evaluation(similarity_coefficient, combining_method: CombiningMethod, save_destination="",
+                      print_results=False, num_threads=-1):
+    print(meta_evaluation.event_processor.translators)
+
+    evaluation = meta_evaluation.evaluate(similarity_coefficient, combining_method,
+                                          num_threads=THREADS if num_threads < 1 else num_threads)
+
+    if save_destination != "":
+        if not exists(dirname(save_destination)):
+            mkdirRecursive(abspath(dirname(save_destination)))
+        if exists(save_destination):
+            os.remove(save_destination)
+        with gzip.open(save_destination, "xb") as f:
+            pickle.dump(evaluation, f)
+
+    if print_results:
+        avg_buggy_in_ranking = sum(
+            len(ranking.buggy_in_ranking) / len(ranking.buggy_methods) for ranking in evaluation.rankings) / len(
+            evaluation.rankings)
+
+        print(len(meta_evaluation.meta_rankings))
+        print(len(evaluation.rankings))
+        print(avg_buggy_in_ranking)
+        print("\n")
+
+        print(evaluation.fraction_top_k_accurate)
+        print(evaluation.avg_recall_at_k)
+        print(evaluation.avg_precision_at_k)
+
 
 if __name__ == "__main__":
     DEFAULT_INPUT = os.path.dirname(os.path.abspath(sys.argv[0])) + "/translated_results.pickle.gz"
@@ -19,21 +52,11 @@ if __name__ == "__main__":
                             help="The file conataining test results")
 
     args = arg_parser.parse_args()
+    combining_method = WeightedCombiningMethod(((LineCoveredEvent, .5), (SDBranchEvent, .1), (SDReturnValueEvent, .2)),
+                                               max, inv_arg)
 
     with gzip.open(args.result_file, "rb") as f:
         meta_evaluation = MetaEvaluation.from_me(pickle.load(f))
 
-    print(meta_evaluation.event_processor.translators)
-    combining_method = WeightedCombiningMethod(((LineCoveredEvent, .5), (SDBranchEvent, .1), (SDReturnValueEvent, .2)), max, inv_arg)
-    evaluation = meta_evaluation.evaluate(OchiaiCoefficient, combining_method, num_threads=THREADS)
-
-    avg_buggy_in_ranking = sum(len(ranking.buggy_in_ranking)/len(ranking.buggy_methods) for ranking in evaluation.rankings) / len(evaluation.rankings)
-
-    print(len(meta_evaluation.meta_rankings))
-    print(len(evaluation.rankings))
-    print(avg_buggy_in_ranking)
-    print("\n")
-
-    print(evaluation.fraction_top_k_accurate)
-    print(evaluation.avg_recall_at_k)
-    print(evaluation.avg_precision_at_k)
+    create_evaluation(OchiaiCoefficient, combining_method, print_results=True,
+                      save_destination=f"./evaluation_{meta_evaluation.meta_rankings[0]._results.project_name}.pickle.gz")
